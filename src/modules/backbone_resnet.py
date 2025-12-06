d3eb2d67f2a6230f6bfa568735d9ea97598198ec
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2025-12-06 11:09:22 +0300

    Create backbone_resnet.py

---
+++ src/modules/backbone_resnet.py
@@ -0,0 +1,21 @@
+import torch
+import torch.nn as nn
+from torchvision.models import resnet50
+
+class BackboneResNet(nn.Module):
+    def __init__(self, pretrained=True):
+        super().__init__()
+        resnet = resnet50(pretrained=pretrained)
+        self.stem = nn.Sequential(resnet.conv1, resnet.bn1, resnet.relu, resnet.maxpool)
+        self.layer1 = resnet.layer1
+        self.layer2 = resnet.layer2
+        self.layer3 = resnet.layer3
+        self.layer4 = resnet.layer4
+
+    def forward(self, x):
+        c1 = self.stem(x)
+        c2 = self.layer1(c1)
+        c3 = self.layer2(c2)
+        c4 = self.layer3(c3)
+        c5 = self.layer4(c4)
+        return c2, c3, c4, c5
